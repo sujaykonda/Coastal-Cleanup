729967f52dac1511b2d03da40faffe111d1c0faf
sujaykonda <kondasujay2@gmail.com>
2022-07-21 18:27:29 -0400

    details stuff

--- App.js
+++ App.js
@@ -2,19 +2,26 @@ import React, { Component } from "react";
 import { Alert, Modal, StyleSheet, Text, Pressable, View, ImageBackground, Button, TouchableOpacity } from "react-native";
 import PollutionFeed from "./PollutionFeed";
 import PollutionForm from "./PollutionForm";
+import PollutionDetails from "./PollutionDetails"
 // Initialize Firebase
 class App extends Component {
     state = {
         formModalVisible: false,
         mapModalVisible: false,
+        detailsModalVisible: false,
+        docid: ""
     };
 
-    setVisible = (f, m) => {
-        this.setState({ formModalVisible: f, mapModalVisible: m });
+    setVisible = (f, m, d) => {
+        this.setState({ formModalVisible: f, mapModalVisible: m, detailsModalVisible: d, docid: this.state.docid });
+    }
+
+    setAll = (f, m, d, docid) => {
+        this.setState({ formModalVisible: f, mapModalVisible: m, detailsModalVisible: d, docid: docid })
     }
 
     render() {
-        const { formModalVisible, mapModalVisible } = this.state
+        const { formModalVisible, mapModalVisible, detailsModalVisible, docid } = this.state
         return (
             <View style={styles.centeredView}>
                 <ImageBackground source={require("./assets/beach-home-page.png")} resizeMode="stretch" style={styles.backgroundImage}>
@@ -23,46 +30,49 @@ class App extends Component {
                         animationType="slide"
                         transparent={true}
                         visible={formModalVisible}
-                        onRequestClose={() => {
-                            this.setVisible(!formModalVisible, mapModalVisible)
-                        }}
                     >
-                        <PollutionForm onClose={() => { this.setVisible(!formModalVisible, mapModalVisible) }} />
+                        <PollutionForm onClose={() => { this.setVisible(false, false, false) }} />
                     </Modal>
 
                     <Modal
                         animationType="slide"
                         transparent={true}
                         visible={mapModalVisible}
-                        onRequestClose={() => {
-                            this.setVisible(formModalVisible, !mapModalVisible);
-                        }}
                     >
-                        <PollutionFeed onClose={() => { this.setVisible(formModalVisible, !mapModalVisible) }} />
+                        <PollutionFeed onDetailsOpen={(docid) => { this.setAll(false, false, true, docid); }}
+                            onClose={() => { this.setVisible(false, false, false) }} />
+                    </Modal>
+
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={detailsModalVisible}
+                    >
+                        <PollutionDetails docid={docid} onClose={() => { this.setVisible(false, false, false) }} />
                     </Modal>
 
                     <Text style={styles.largeText}>Together, we can do it!</Text>
                     <View style={styles.buttonContainer}>
                         <TouchableOpacity title="File Pollution Report" style={styles.button} activeOpacity={0.8}
                             onPress={() => {
-                                this.setVisible(true, mapModalVisible);
+                                this.setVisible(true, false, false);
                             }}>
                             <Text style={styles.mainButtonText}>Report Pollution</Text>
                         </TouchableOpacity>
                         <TouchableOpacity title="Volunteer Sign Up" style={styles.button} activeOpacity={0.8}
                             onPress={() => {
-                                this.setVisible(formModalVisible, true);
+                                this.setVisible(false, true, false);
                             }}>
                             <Text style={styles.mainButtonText}>Volunteer Sign Up</Text>
                         </TouchableOpacity>
                     </View>
                     <View Style={styles.buttonContainer}>
-                        <TouchableOpacity title="Contact" style = {styles.button2} activeOpacity={0.8}
-                             onPress={() => {
+                        <TouchableOpacity title="Contact" style={styles.button2} activeOpacity={0.8}
+                            onPress={() => {
                                 //  this.setVisible(formModalVisible, true);
                                 // create contact page and put here
                             }}>
-                            <Text style = {styles.mainButtonText}>Contact</Text>
+                            <Text style={styles.mainButtonText}>Contact</Text>
                         </TouchableOpacity>
                     </View>
                 </ImageBackground>
